b620f510bfcb6725e76e902040b2ddf304ee7b8c
Jorgealfue10 <jorgealonsodelafuente@gmail.com>
2023-12-27 15:55:48 +0100

    ue

--- get_errors.f90
+++ get_errors.f90
@@ -1,15 +1,15 @@
 program errors 
     implicit none
     integer :: i,j,npoints
-    real(kind=8) :: r12,r13,r23,e,de,en,aux
+    real(kind=8) :: r12,r13,r23,e,de,en,aux,eh
     real(kind=8) :: der(3)
     character(len=1) :: diattype
     integer :: io
 
     interface
-        subroutine fit3d(r1,r2,r3,e,der)
+        subroutine fit3d(r1,r2,r3,e,eh,der)
             implicit none
-            real(8) :: r1,r2,r3,e
+            real(8) :: r1,r2,r3,eh,e
             real(8) :: der(3)
         end subroutine
         subroutine diat12(r,e,der)
@@ -30,19 +30,27 @@ program errors
     write(*,*) "For the diatomic molecule."
     write(*,fmt='(A19)',advance='no') "Homonuclear (y/n): "
     read(*,*) diattype
-    write(*,*) "-----------------------------------------------"
     write(*,*)
+    write(*,*) "-----------------------------------------------"
 
+    i=0
     do
-        read(15,*,iostat=io) r12,r13,r23,e,aux,aux,aux
+        read(15,*,iostat=io) r12,r13,r23,e!,aux,aux,aux
         if (io.ne.0) exit
-        call get_ic(rg,rp,theta,r12,r13,r23,diattype)
-        call fit3d(r12,r13,r23,en,der)
-        if (abs(en-e)*219474>20) then
-            write(17,*) r12,r13,r23,e
+        ! call get_ic(rg,rp,theta,r12,r13,r23,diattype)
+        call fit3d(r12,r13,r23,en,eh,der)
+        !e=eh+e
+        if (abs(en-e)*219474.ge.2000) then
+            write(17,*) r12,r23,r13,e,en,abs(en-e)*219474
+            print*,i
         else
-            write(16,*) r12,r13,r23,e
+            write(16,*) r12,r23,r13,e
+            print*,"A",i
         endif
+        i=i+1
     enddo
+    close(15)
+    close(16)
+    close(17)
 
 end program errors
